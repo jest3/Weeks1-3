cbe7fa11de934078ee72e04fed172c42c4050cde
Kira Chen <chenki@shernet.sheridancollege.ca>
2025-01-15 10:10:41 -0500

    bouncing ball resets when space is pressed

--- Assets/Scripts/BouncingBall.cs
+++ Assets/Scripts/BouncingBall.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BouncingBall : MonoBehaviour
 {
 
     float speedX = 0.01f;
     float speedY = 0.01f;
+    Boolean KeyPressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +35,21 @@ public class BouncingBall : MonoBehaviour
         if(screenPos.y < 0 || screenPos.y > Screen.height)
         {
             speedY = speedY * -1;
+        }
+
+        // reset ball position when spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            KeyPressed = true;
+        } 
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            KeyPressed = false;
+        }
+        if(KeyPressed == true)
+        {
+            circlePos.x = 0;
+            circlePos.y = 0;
         }
 
         transform.position = circlePos;
