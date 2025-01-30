7ea39e300f833f858e87c7c6e6519dd7ad499f56
Kira Chen <chenki@shernet.sheridancollege.ca>
2025-01-30 02:04:04 -0500

    dragon can be moved and rotated using WASD and Arrow keys, temprorarily stops when reaches the edge of the screen. Lantern loops up and down between two positions using lerp and animation curve.

--- Assets/Scripts/DragonHead.cs
+++ Assets/Scripts/DragonHead.cs
@@ -6,10 +6,10 @@ using UnityEngine.Animations;
 public class DragonHead : MonoBehaviour
 {
     //sets speed of dragon movement on the horizontal and vertical axis
-    public float moveSpeed = 10f;
+    public float moveSpeed = 10;
     //sets the rotation speed of the dragon on the horizontal axis
-    public float rotateSpeed = 65f;
-        
+    public float rotateSpeed = 65;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,19 +21,25 @@ public class DragonHead : MonoBehaviour
     {
 
         Vector3 pos = transform.position;
-        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
-
-        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        //changes the dragons position from world space to screen space, so that the dragons position on the screen can be checked in the if statement
+        Vector2 dragonPos = Camera.main.WorldToScreenPoint(pos);
+        
+        //checks if the dragon position reaches the edge of the screen on the x axis
+        if (dragonPos.x < 0 || dragonPos.x > Screen.width)
         {
-            moveSpeed = 0;
+            //sets the move Speed to 0.5
+            moveSpeed = 0.9f;
+        } 
+            //otherwise, when dragon position is not at the edge of the screen, set its speed back to 10
+        else
+        {
+            moveSpeed = 10;
         }
-
-        transform.position = pos;
 
         //moves dragon with arrow keys or WASD on horizontal and vertical axis
         transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime, 0);
+        
         //Rotates dragon with left and right arrow keys, and the A and D keys, on horizontal axis
         transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime);
-
-}
+    }
 }
--- Assets/Scripts/Lantern.cs
+++ Assets/Scripts/Lantern.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Lantern : MonoBehaviour
 {
     public AnimationCurve lanternCurve;
 
-    [Range(0f, 1f)]
+    //sets range of animation curve
+    [Range(0, 2)]
+    //represents total time
     public float t;
 
-    
+    //allows the lantern to have a set starting position and an ending position while lerping
     public Transform pos1;
     public Transform pos2;
-
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,16 @@ public class Lantern : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //total time in seconds increases every frame
+        t += Time.deltaTime;
 
+        //if the animation curve reaches its end (2 seonds), reset to 0 to start from beginning again, loops the animation curve
+        if (t > 2)
+        {
+            t = 0;
+        }
+
+        //transforms position of the lantern to move from the set position 1 to position 2
         transform.position = Vector2.Lerp(pos1.position, pos2.position, lanternCurve.Evaluate(t));
 
     }
